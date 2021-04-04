87e07b533cdedf87b34263bc4167456286380d4c
Vojtěch Loskot <Loskot@porg.cz>
2021-04-04 12:42:22 +0200

    fix

--- calendar.jsx
+++ calendar.jsx
@@ -31,40 +31,50 @@ const DAYNAMES = [
 ]
 const MONTHNAMES = ["Les jours complémentaires", "I Vendémiaire", "II Brumaire", "III Frimaire", "IV Nivôse", "V Pluviôse", "VI Ventôse", "VII Germinal", "VIII Floréal", "IX Prairial", "X Messidor", "XI Thermidor", "XII Fructidor",
 ]
-export const render = () => {
-    let date = new Date();
 
-    let leapYear = date.getFullYear() % 4 == 0 && date.getFullYear() % 100 > 0;
+const getData = () => {
+    var date = new Date();
+
+    var leapYear = date.getFullYear() % 4 == 0 && date.getFullYear() % 100 > 0;
 
     var start = new Date(date.getFullYear(), 0, 0);
     var diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
     var oneDay = 1000 * 60 * 60 * 24;
     var dayOfyear = Math.floor(diff / oneDay);
 
-    let offset = 11;
+    var offset = 11;
     if (dayOfyear > 259 && !leapYear) {
         offset = 6;
     } else if (dayOfyear > 259 && leapYear) {
         offset = 5;
     }
 
-    let dayOfTheMonth = (date.getDay() + offset) % 30;
+    var dayOfTheMonth = (date.getDay() + offset) % 30;
+
     if (dayOfTheMonth == 0) {
         dayOfTheMonth = 30
     }
-    let monthNumber = Math.ceil(((dayOfyear + 102) / 30) % 13);
+    var monthNumber = Math.ceil(((dayOfyear + 102) / 30) % 13);
 
-    let dayoftheweek = dayofTheMonth % 10;
-    let dayName = DAYNAMES[dayoftheweek];
-    let montName = MONTHNAMES[monthNumber]
+    var dayoftheweek = dayofTheMonth % 10;
+    var dayName = DAYNAMES[dayoftheweek];
+    var monthName = MONTHNAMES[monthNumber]
+    return {
+        day: dayOfTheMonth,
+        dayName: dayName,
+        monthName: monthName
+    }
+}
 
+export const initialState = getData();
 
+export const render = ({ day, dayName, monthName }, dispatch) => {
     return <div className={text}>
         <div className={wrapper}>
-            <h1 className={bigText}>{dayOfTheMonth}</h1>
+            <h1 className={bigText}>{day}</h1>
             <div className={inner}>
                 <p>{dayName}</p>
-                <p className={bottom}>{montName}</p>
+                <p className={bottom}>{monthName}</p>
             </div>
         </div>
     </div>
