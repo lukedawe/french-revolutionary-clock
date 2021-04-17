77b0f3632c039c99d3c6f81e01c5c528e7902dbb
Vojtěch Loskot <Loskot@porg.cz>
2021-04-17 13:06:52 +0200

    Revert "fix"
    This reverts commit 87e07b533cdedf87b34263bc4167456286380d4c.

--- calendar.jsx
+++ calendar.jsx
@@ -32,18 +32,17 @@ const DAYNAMES = [
 ]
 const MONTHNAMES = ["Les jours complémentaires", "I Vendémiaire", "II Brumaire", "III Frimaire", "IV Nivôse", "V Pluviôse", "VI Ventôse", "VII Germinal", "VIII Floréal", "IX Prairial", "X Messidor", "XI Thermidor", "XII Fructidor",
 ]
+export const render = () => {
+    let date = new Date();
 
-const getData = () => {
-    var date = new Date();
-
-    var leapYear = date.getFullYear() % 4 == 0 && date.getFullYear() % 100 > 0;
+    let leapYear = date.getFullYear() % 4 == 0 && date.getFullYear() % 100 > 0;
 
     var start = new Date(date.getFullYear(), 0, 0);
     var diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
     var oneDay = 1000 * 60 * 60 * 24;
     var dayOfyear = Math.floor(diff / oneDay);
 
-    var offset = 11;
+    let offset = 11;
     if (dayOfyear > 259 && !leapYear) {
         offset = 6;
     } else if (dayOfyear > 259 && leapYear) {
@@ -56,7 +55,7 @@ const getData = () => {
     if (dayOfTheMonth == 0) {
         dayOfTheMonth = 30
     }
-    var monthNumber = Math.ceil(((dayOfyear + 102) / 30) % 13);
+    let monthNumber = Math.ceil(((dayOfyear + 102) / 30) % 13);
 
     var dayoftheweek = dayOfTheMonth % 10;
     var dayName = DAYNAMES[dayoftheweek];
@@ -68,22 +67,20 @@ const getData = () => {
     }
 }
 
-export const initialState = getData();
 
-export const render = ({ day, dayName, monthName }, dispatch) => {
-    return <div className={text}>
-        <Global styles={{
-            '@font-face': {
-                fontFamily: 'Folio',
-                src: 'url(\'./Folio-Bold.otf\')',
-            },
-        }} />
-        <div className={wrapper}>
-            <h1 className={bigText}>{day}</h1>
-            <div className={inner}>
-                <p>{dayName}</p>
-                <p className={bottom}>{monthName}</p>
-            </div>
+return <div className={text}>
+    <Global styles={{
+        '@font-face': {
+            fontFamily: 'Folio',
+            src: 'url(\'./Folio-Bold.otf\')',
+        },
+    }} />
+    <div className={wrapper}>
+        <h1 className={bigText}>{dayOfTheMonth}</h1>
+        <div className={inner}>
+            <p>{dayName}</p>
+            <p className={bottom}>{montName}</p>
         </div>
     </div>
+</div>
 }
